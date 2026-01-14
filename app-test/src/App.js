54b2fb124f22baf7ba15c617d7a827784e104381
paula <paula.beatriz@upe.br>
2026-01-14 12:08:26 -0300

    learning about components

--- app-test/src/App.js
+++ app-test/src/App.js
@@ -3,9 +3,21 @@
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Job salary={90000} positon="Senior SDE" company="Amazon" />
+      <Job salary={12000} positon="Juniro SDE" company="Google" />
+      <Job salary={10000} positon="Project Manager" company="Netflix" />
     </div>
   );
+}
+
+const Job = (props) => {
+  return (
+    <div>
+      <h1>{props.salary}</h1>
+      <h1>{props.positon}</h1>
+      <h1>{props.company}</h1>
+    </div>
+  )
 }
 
 export default App;
