0c63687043bd6cea5b5c27f70aed59d1de205fe3
paula <paula.beatriz@upe.br>
2026-01-20 14:45:19 -0300

    fetching data from an API

--- app-test/src/App.js
+++ app-test/src/App.js
@@ -1,51 +1,25 @@
+import {useState } from 'react';
 import './App.css';
-import { useState } from 'react';
-import {Task} from "./Task"
+import Axios from "axios";
 
 function App() {
 
-  const[todoList, setTodoList] = useState([]);
-  const[newTask, setNewTask] = useState("");
+  const [generatedExcuse, setGeneratedExcuse] = useState("");
 
-  const handleChange = (event) => {
-    setNewTask(event.target.value);
+  const fetchExcuse = (excuse) => {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then((res) => {
+    setGeneratedExcuse(res.data[0].excuse);
+  });
   }
-
-  const addTask = () => {
-    const task = {
-      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-      completed: false,
-    };
-    setTodoList([...todoList, task]); 
-  }
-
-  const deleteTask = (id) => {
-    setTodoList(todoList.filter((task) => task.id !== id));
-  }
-
-  const completeTask = (id) => {
-    setTodoList(todoList.map((task) => {
-      if (task.id === id){
-        return {...task, completed: true};
-      }else{
-        return task;
-      }
-    }))
-  } 
 
   return (
     <div className="App">
-      <div className='addTask'>
-        <input onChange={handleChange} />
-        <button onClick={addTask} >Add Task</button>
-        
-      </div>
-      <div className='list'>
-        {todoList.map((task) => {
-         return <Task taskName={task.taskName} id={task.id} completed={task.completed} deleteTask={deleteTask} completeTask={completeTask}/>;
-        })}
-      </div>
+      <h1>Generate An Excuse</h1>
+      <button onClick={() => fetchExcuse("party")}> Party </button>
+      <button onClick={() => fetchExcuse("family")}> Family </button>
+      <button onClick={() => fetchExcuse("office")}> Office </button>
+
+      <p>{generatedExcuse}</p>
     </div>
   );
 }
