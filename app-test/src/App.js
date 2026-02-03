05868db483719207afb37b9a4d80bca2ba97351a
paula <paula.beatriz@upe.br>
2026-02-03 10:31:58 -0300

    custom hooks

--- app-test/src/App.js
+++ app-test/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { Form } from './components/Form';
+import { Cat } from './Cat';
 
 function App() {
   return (
-    <div>
-      <Form />
+    <div className="App">
+      <Cat/>
     </div>
   );
 }
